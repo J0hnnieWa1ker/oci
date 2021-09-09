1c364dc9890184fa43b60c866cfefba61a2c82df
J0hnnieWa1ker <j0hnniewa1ker@icloud.com>
2021-09-09 02:11:26 -0400

    configure https://developers.redhat.com/courses/red-hat-linux/open-lab

---
+++ oci.sh
@@ -0,0 +1,5 @@
+git config --global user.name "J0hnnieWa1ker"
+curl -O https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-client-linux.tar.gz
+tar -xvf openshift-client-linux.tar.gz 
+mv oc /usr/bin/.
+mv kubectl /usr/bin/.
